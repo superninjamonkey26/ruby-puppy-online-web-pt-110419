7a1c0a461f6a3a2e9bbc3bbea0edd38c69bca7b5
Monkey Mendel <stephanie.mendel26@gmail.com>
2019-12-07 18:58:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,15 +12,11 @@ class Dog
   def self.clear_all
     @@all.clear
   end
-  
-  
   def self.print_all
     @@all.each do |dog|
       puts "#{dog.name}"
     end
   end
-  
-  
   def save
     @@all << self
   end
